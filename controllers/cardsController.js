ba18f6d246b9052d9919f1b97718db4278bee30a
Fabian Juren <fjuren@gmail.com>
2023-02-26 14:23:10 -0800

    Add query function to controller to pull all card data from db

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -2,11 +2,16 @@ const async = require('async');
 const { body } = require('express-validator');
 const Cards = require('../models/cards');
 
+// Display the Community Creation homepage. Contains:
+// Total count of cards, all the cards and their data,
 exports.index = (req, res) => {
   async.parallel(
     {
       cardCount(cb) {
         Cards.countDocuments({}, cb);
+      },
+      allCardData(cb) {
+        Cards.find({}, cb);
       },
     },
     (err, result) => {
--- routes/communityCreations.js
+++ routes/communityCreations.js
@@ -6,4 +6,18 @@ const cardsController = require('../controllers/cardsController');
 // GET Homepage (Community Creations page)
 router.get('/', cardsController.index);
 
+// GET request for viewing individual cards
+
+// GET request for creating a new card
+
+// POST request for creatign a new card
+
+// GET request for deleteing a card
+
+// POST request for deleting a card
+
+// GET request for editing a card
+
+// POST request for editing a card
+
 module.exports = router;
