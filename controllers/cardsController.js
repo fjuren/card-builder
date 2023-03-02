b0a62e7ff90876740c5a310293c7212e16006a0d
Fabian Juren <fjuren@gmail.com>
2023-03-01 20:49:35 -0800

    Add data to index view

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -27,36 +27,57 @@ exports.index = (req, res) => {
     },
     (err, result) => {
       const name = [];
+      const hp = [];
+      const type = [];
+      const description = [];
+      const attack_1 = [];
+      const attack_1_description = [];
+      const damage_1 = [];
       const cost_1 = [];
+      const attack_2 = [];
+      const attack_2_description = [];
+      const damage_2 = [];
+      const cost_2 = [];
+      const weakness = [];
+      const resistance = [];
+      const retreat_cost = [];
+
       result.allCardData.forEach((c) => {
-        console.log(`name: ${c.name}-------------------`);
         name.push(c.name);
-        console.log(`type ${c.type.type}`);
+        hp.push(c.hp);
+        type.push(c.type.type);
+        description.push(c.description);
+        attack_1.push(c.attack_1);
+        attack_1_description.push(c.attack_1_description);
+        damage_1.push(c.damage_1);
 
         // Query cost 1
         c.cost_1.forEach((c1) => {
-          console.log(`cost1 ${c1.type}`);
           cost_1.push(c1.type);
         });
 
+        attack_2.push(c.attack_2);
+        attack_2_description.push(c.attack_2_description);
+        damage_2.push(c.damage_2);
+
         // Query cost 2
         c.cost_2.forEach((c2) => {
-          console.log(`cost2 ${c2.type}`);
+          cost_2.push(c2.type);
         });
 
         // Query weaknesses
         c.weakness.forEach((w) => {
-          console.log(`weakness ${w.type}`);
+          weakness.push(w.type);
         });
 
         // Query resistance
         c.resistance.forEach((r) => {
-          console.log(`resistance ${r.type}`);
+          resistance.push(r.type);
         });
 
         // Query retreate cost
         c.retreat_cost.forEach((rc) => {
-          console.log(`retreatCost ${rc.type}`);
+          retreat_cost.push(rc.type);
         });
       });
 
@@ -66,7 +87,21 @@ exports.index = (req, res) => {
         title: 'Community Creations',
         error: err,
         cardCount: result.cardCount,
-        allCardData: result.allCardData,
+        name,
+        hp,
+        type,
+        description,
+        attack_1,
+        attack_1_description,
+        damage_1,
+        cost_1,
+        attack_2,
+        attack_2_description,
+        damage_2,
+        cost_2,
+        weakness,
+        resistance,
+        retreat_cost,
       });
     }
   );
